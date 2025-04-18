1ab02532a93a0993319b4ed329ccdc6422e186b5
Narazaka <info@narazaka.net>
2025-04-18 23:05:56 +0900

    Revert "impulse=0.05"
    This reverts commit e0f9e9e26816629a7db5b7abbc0cda032fa7a99c.

--- Editor/VRC3CVR.cs
+++ Editor/VRC3CVR.cs
@@ -604,7 +604,7 @@ public class VRC3CVR : EditorWindow
         }
         public string MenuName
         {
-            get => addActionMenuModAnnotations && IsButton ? name + "<impulse=0.05>" : name;
+            get => addActionMenuModAnnotations && IsButton ? name + "<impulse>" : name;
         }
         public bool IsButton
         {
