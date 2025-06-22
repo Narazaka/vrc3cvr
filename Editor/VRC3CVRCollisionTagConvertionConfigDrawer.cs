b37443348dcfc968347cd30044c31cab2f312ba5
Narazaka <info@narazaka.net>
2025-06-22 22:11:38 +0900

    fix drawer

--- Editor/VRC3CVRCollisionTagConvertionConfigDrawer.cs
+++ Editor/VRC3CVRCollisionTagConvertionConfigDrawer.cs
@@ -6,9 +6,13 @@ public class VRC3CVRCollisionTagConvertionConfigDrawer : PropertyDrawer
 {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        EditorGUI.BeginProperty(position, label, property);
-        EditorGUI.PropertyField(position, property, label, true);
+        var pos = EditorGUI.IndentedRect(position);
+        var indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+        EditorGUI.BeginProperty(pos, label, property);
+        EditorGUI.PropertyField(pos, property, label, true);
         EditorGUI.EndProperty();
+        EditorGUI.indentLevel = indent;
     }
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
